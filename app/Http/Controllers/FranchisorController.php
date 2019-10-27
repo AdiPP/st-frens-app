a45d43af20a886cc3716987f4c9410299ad17a42
AxlAdilla <axladilla@gmail.com>
2019-10-27 13:47:12 +0700

    errr okay

---
+++ app/Http/Controllers/FranchisorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FranchisorController extends Controller
+{
+    //
+
+    /***
+     * Modul Franchisor
+        - Konfirmasi Agen
+        - Monitoring Agen
+        - Laporan Produk
+     * 
+     */
+}
---
+++ app/Http/Controllers/PemesananController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PemesananController extends Controller
+{
+    //
+
+    /**
+     * Modul Pemesanan
+     *  - Pemesanan
+     *  - Konfirmasi Pesanan
+     *  - Riwayat Pesanan
+     * 
+     */
+}
